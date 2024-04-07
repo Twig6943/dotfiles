f40969dc60c6922b29a547c45828082ee6bfe3e7
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-08 02:51:49 +0300

    Update endavour.sh

--- endavour.sh
+++ endavour.sh
@@ -1,1 +1,9 @@
 echo placeholder
+sudo pacman -Syu
+sudo pacman -S kitty
+sudo pacman -S hyprland
+sudo pacman -S wofi
+sudo pacman -S pavucontrol
+sudo pacman -S yay
+sudo pacman -S flatpak
+yay -S cava
