03602e49d5fca81d61f50b11a0d817923a99b4ac
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-08 03:48:47 +0300

    Update endavour.sh

--- endavour.sh
+++ endavour.sh
@@ -1,4 +1,5 @@
-echo placeholder
+echo "Cancel the script if u want u have 5 seconds"
+sleep 5
 sudo pacman -Syu
 sudo pacman -S kitty
 sudo pacman -S fastfetch
@@ -13,3 +14,12 @@ sudo pacman -S playerctl
 sudo pacman -S waybar
 yay -S cava --noconfirm
 yay -S appimagelauncher --noconfirm
+#MangohudGoverlay
+cd /home/$USER/Downloads
+git clone https://github.com/Twig6943/MangoHudandGoverlayInstaller
+sudo cp /home/$USER/Downloads/MangoHudandGoverlayInstaller/desktopfile.sh /home/$USER/Downloads
+sudo cp /home/$USER/Downloads/MangoHudandGoverlayInstaller/uninstalldesktopfile.sh /home/$USER/Downloads
+sudo cp /home/$USER/Downloads/MangoHudandGoverlayInstaller/install.sh /home/$USER/Downloads
+sudo cp /home/$USER/Downloads/MangoHudandGoverlayInstaller/uninstall.sh /home/$USER/Downloads
+/home/$USER/Downloads/MangoHudandGoverlayInstaller/install.sh
+echo "Installation is done"
