453c7f2c8e3e074b406f4b213fdfe4cabbb3d6da
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-10 21:45:48 +0300

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -22,6 +22,7 @@ sudo apt install playerctl
 sudo apt install wofi
 sudo apt install grimshot
 sudo apt install gammastep
+sudo apt install swaylock
 #Fastfetch
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.8.10/fastfetch-linux-amd64.deb -O /home/$USER/Downloads/fastfetch.deb
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb -O /home/$USER/Downloads/appimage.deb
