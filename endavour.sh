4eb399bec4fb480471621688cfd1d7085b651b50
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-18 03:03:00 +0300

    Update endavour.sh

--- endavour.sh
+++ endavour.sh
@@ -2,22 +2,26 @@ echo "Cancel the script if u want u have 5 seconds"
 sleep 5
 #Pacman
 sudo pacman -Syu
-sudo pacman -S alacritty
 sudo pacman -S qbittorrent
 sudo pacman -S fastfetch
-sudo pacman -S gnome
+#sudo pacman -S gnome
+
+#Sway stuff
+sudo pacman -S kitty
 sudo pacman -S sway
 sudo pacman -S swaybg
+sudo pacman -S swaylock
 sudo pacman -S xdg-desktop-portal-wlr
-sudo pacman -S gammastep 
+sudo pacman -S gammastep
+sudo pacman -S waybar
+sudo pacman -S playerctl
 sudo pacman -S wofi
+#Sway stuff
 sudo pacman -S pavucontrol
 sudo pacman -S yay
 sudo pacman -S flatpak
 sudo pacman -S lxappearance
 sudo pacman -S thunar
-sudo pacman -S playerctl
-sudo pacman -S waybar
 sudo pacman -S steam
 sudo pacman -S wine
 sudo pacman -S winetricks
@@ -31,8 +35,8 @@ systemctl --user restart xdg-desktop-portal
 mkdir /home/$USER/.config/qBittorrent/
 wget https://github.com/maboroshin/qBittorrentDarktheme/raw/master/ICEBERG.qbtheme -O /home/$USER/.config/qBittorrent/ICEBERG.qbtheme
 #Wallpaper
-wget https://github.com/hyprwm/Hyprland/blob/main/assets/wall2.png?raw=true -O wp.png
-sudo mkdir /home/$USER/.config/sway/
+wget https://github.com/hyprwm/Hyprland/blob/main/assets/wall2.png?raw=true -O /home/$USER/.config/sway/wp2.png
+sudo mkdir /home/$USER/.config/sway
 sudo cp wp.png /home/$USER/.config/sway/wp.png
 sudo rm wp.png
 #Yay
@@ -42,18 +46,31 @@ yay -S grimshot-bin-sway --noconfirm
 yay -S wdisplays --noconfirm
 yay -S noisetorch-bin --noconfirm
 #Flatpak
-sudo flatpak install io.github.spacingbat3.webcord
+sudo flatpak install xyz.armcord.ArmCord
 sudo flatpak install com.discordapp.Discord
 sudo flatpak install org.gimp.GIMP
 sudo flatpak install org.vinegarhq.Vinegar
 sudo flatpak install com.github.unrud.VideoDownloader
 sudo flatpak install me.hyliu.fluentreader
 sudo flatpak install com.heroicgameslauncher.hgl
+sudo flatpak install io.github.ungoogled_software.ungoogled_chromium
+sudo flatpak install io.github.TheWisker.Cavasik
+sudo flatpak install com.github.tchx84.Flatseal
+sudo flatpak install org.prismlauncher.PrismLauncher
+sudo flatpak install org.duckstation.DuckStation
+sudo flatpak install com.github.eneshecan.WhatsAppForLinux
+#sudo flatpak install flathub org.wezfurlong.wezterm
 #sudo flatpak install org.deluge_torrent.deluge
+#Brawl // Animation 
+sudo flatpak install org.synfig.SynfigStudio
+sudo flatpak install org.inkscape.Inkscape
+sudo flatpak install com.jpexs.decompiler.flash
 #Clone dotfiles
 git clone https://github.com/Twig6943/dotfiles/
 #Rename kde crap
 sudo mv /usr/bin/kwalletd5 /usr/bin/kwalletd55
 sudo mv /usr/bin/kwalletd66 /usr/bin/kwalletd66
 sudo mv /usr/bin/kwallet-query /usr/bin/kwallet-queryy
+
+git clone https://github.com/Twig6943/dotfiles
 echo "Installation is done"
