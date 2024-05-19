be1eb5273e69d03535f2420d82c2e9765b36f190
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-19 18:06:13 +0300

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -28,6 +28,7 @@ sudo apt install swaybg
 sudo apt install swayidle
 sudo apt install swaylock
 sudo apt install xdg-desktop-portal-wlr
+sudo apt install wdisplays
 sudo apt install waybar 
 sudo apt install playerctl
 sudo apt install wofi
