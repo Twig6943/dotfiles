521bba7c4aebd17bbf998476e06012a60c629ccf
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-21 11:53:03 +0300

    Update endavour.sh

--- endavour.sh
+++ endavour.sh
@@ -11,26 +11,6 @@ sudo pacman -Sy git
 sudo pacman -S qbittorrent
 sudo pacman -S fastfetch
 #sudo pacman -S gnome
-
-
-#Nvidia Stuff (Disabled)
-#git clone https://github.com/Frogging-Family/nvidia-all.git
-#cd nvidia-all
-#makepkg -si
-
-
-
-#Sway stuff
-sudo pacman -S kitty
-sudo pacman -S sway
-sudo pacman -S swaybg
-sudo pacman -S swaylock
-sudo pacman -S xdg-desktop-portal-wlr
-sudo pacman -S gammastep
-sudo pacman -S waybar
-sudo pacman -S playerctl
-sudo pacman -S wofi
-#Sway stuff
 sudo pacman -S pavucontrol
 sudo pacman -S yay
 sudo pacman -S flatpak
@@ -44,15 +24,31 @@ sudo pacman -S htop
 sudo pacman -S cmatrix
 sudo pacman -S goverlay
 sudo pacman -S xfce4-taskmanager
-#Pacman end
+
+#Nvidia Stuff (Disabled)
+#git clone https://github.com/Frogging-Family/nvidia-all.git
+#cd nvidia-all
+#makepkg -si
+
+#Sway stuff
+sudo pacman -S kitty
+sudo pacman -S sway
+sudo pacman -S swaybg
+sudo pacman -S swaylock
+sudo pacman -S xdg-desktop-portal-wlr
+sudo pacman -S gammastep
+sudo pacman -S waybar
+sudo pacman -S playerctl
+sudo pacman -S wofi
+
 systemctl --user restart xdg-desktop-portal
 mkdir /home/$USER/.config/qBittorrent/
 wget https://github.com/maboroshin/qBittorrentDarktheme/raw/master/ICEBERG.qbtheme -O /home/$USER/.config/qBittorrent/ICEBERG.qbtheme
 #Wallpaper
-sudo mkdir /home/$USER/.config/sway
-sudo cp wp.png /home/$USER/.config/sway/wp.png
+sudo mkdir /home/$USER/wallpapers
+sudo cp wp.png /home/$USER/wallpapers
 sudo rm wp.png
-wget https://github.com/hyprwm/Hyprland/blob/main/assets/wall2.png?raw=true -O /home/$USER/.config/sway/wp2.png
+wget https://github.com/hyprwm/Hyprland/blob/main/assets/wall2.png?raw=true -O /home/$USER/wallpapers/wp2.png
 #Yay
 yay -S cava --noconfirm
 yay -S appimagelauncher --noconfirm
@@ -74,24 +70,29 @@ sudo flatpak install org.prismlauncher.PrismLauncher
 sudo flatpak install io.mrarm.mcpelauncher
 sudo flatpak install org.duckstation.DuckStation
 sudo flatpak install com.github.eneshecan.WhatsAppForLinux
-
-#sudo flatpak install flathub org.wezfurlong.wezterm
-#sudo flatpak install org.deluge_torrent.deluge
-
 #Brawl // Animation 
 sudo flatpak install org.synfig.SynfigStudio
 sudo flatpak install org.inkscape.Inkscape
 sudo flatpak install com.jpexs.decompiler.flash
+
+#sudo flatpak install flathub org.wezfurlong.wezterm
+#sudo flatpak install org.deluge_torrent.deluge
+
+#AppImage Package manager
+curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s
+zap init
+zap install --github --from Heroic-Games-Launcher/HeroicGamesLauncher
+zap install --github --from minecraft-linux/appimage-builder
+zap install --github --from VSCodium/vscodium
+
 #Clone dotfiles
-git clone https://github.com/Twig6943/dotfiles/
+cd /home/$USER
+git clone https://github.com/Twig6943/dotfiles
+
 #Rename kde crap
 sudo mv /usr/bin/kwalletd5 /usr/bin/kwalletd55
 sudo mv /usr/bin/kwalletd66 /usr/bin/kwalletd66
 sudo mv /usr/bin/kwallet-query /usr/bin/kwallet-queryy
-
-#Clone the repository
-git clone https://github.com/Twig6943/dotfiles
-
 
 echo "Installation is done"
 sleep 5
