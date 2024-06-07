cb26af00fe66a8a1d944fd323c6943f13b308c59
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-06-07 15:30:24 +0300

    Update endavour.sh

--- endavour.sh
+++ endavour.sh
@@ -52,7 +52,6 @@ sudo mkdir /home/$USER/wallpapers
 wget https://github.com/hyprwm/Hyprland/blob/main/assets/wall2.png?raw=true -O /home/$USER/wallpapers/wp2.png
 #Yay
 yay -S cava --noconfirm
-yay -S appimagelauncher --noconfirm
 yay -S grimshot-bin-sway --noconfirm
 yay -S wdisplays --noconfirm
 yay -S noisetorch-bin --noconfirm
