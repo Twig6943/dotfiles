a26d787b376cdea6d4de2a6d95ec9c6d39c228d0
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-12-29 21:43:33 +0300

    Update EndeavourOS.sh

--- InstallScripts/EndeavourOS.sh
+++ InstallScripts/EndeavourOS.sh
@@ -30,9 +30,10 @@ rebos config init
 cd /home/$USER
 git clone https://github.com/Twig6943/dotfiles
 
-#Wallpaper
+#Wallpapers
 sudo mkdir /usr/share/wallpapers/Custom/
-sudo wget https://lemmy.today/pictrs/image/ff5c85e4-3df6-452b-bb4c-e2abf4be3ae5.jpeg -O /usr/share/wallpapers/Custom/wall1.png
+sudo wget https://lemmy.today/pictrs/image/ff5c85e4-3df6-452b-bb4c-e2abf4be3ae5.jpeg -O /usr/share/wallpapers/Custom/Stolas.png
+sudo wget https://github.com/hyprwm/Hyprland/blob/main/assets/install/wall2.png?raw=true -O /usr/share/wallpapers/Custom/hyprchan.png
 
 #Create /crap/ folder 
 sudo mkdir /crap
