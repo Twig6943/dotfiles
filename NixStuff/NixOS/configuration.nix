fe9cf06fb3a96fe405be14b7a6cdde8312b4f0a7
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-12-30 18:38:01 +0300

    Update configuration.nix

--- NixStuff/NixOS/configuration.nix
+++ NixStuff/NixOS/configuration.nix
@@ -102,8 +102,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+vim
+curl
+wget
+flatpak
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
