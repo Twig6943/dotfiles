c242a0ec9c47ad9c4cf560b18cf0de4b2a3f2a56
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-01-05 11:34:55 +0300

    Update configuration.nix

--- NixStuff/NixOS/configuration.nix
+++ NixStuff/NixOS/configuration.nix
@@ -11,9 +11,9 @@
     ];
 
   # Bootloader.
-#boot.loader.grub.enable = true;
-#boot.loader.grub.device = "/dev/sda";
-#boot.loader.grub.useOSProber = true;
+boot.loader.grub.enable = true;
+boot.loader.grub.device = "/dev/sda";
+boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
