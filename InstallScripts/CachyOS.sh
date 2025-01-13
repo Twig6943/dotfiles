2e5c9c73794eff29ad8cc36156f934955f4c8142
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-01-13 17:03:38 +0300

    Create CachyOS.sh

---
+++ InstallScripts/CachyOS.sh
@@ -0,0 +1,44 @@
+read -n 1 -s -p "Please update via CachyOS-hello first."
+
+echo "Starting within 10 seconds"
+
+sleep 10
+
+#Install flatpak
+sudo pacman -S flatpak --noconfirm
+
+#Extra flatpak installation folder (CHECK "NOTES/README.MD" FOR MORE INFO)
+sudo mkdir -p /etc/flatpak/installations.d
+sudo touch /etc/flatpak/installations.d/extra.conf
+
+#Install AppImage stuff
+mkdir /home/$USER/Applications
+
+#Install YaY
+paru -S yay-bin --noconfirm
+
+#Install AM
+yay -S am --noconfirm
+
+#Rebos
+am -i rebos
+rebos setup
+rebos config init
+
+#Clone dotfiles
+cd /home/$USER
+git clone https://github.com/Twig6943/dotfiles
+
+#Wallpapers
+sudo mkdir /usr/share/wallpapers/Custom/
+sudo wget https://lemmy.today/pictrs/image/ff5c85e4-3df6-452b-bb4c-e2abf4be3ae5.jpeg -O /usr/share/wallpapers/Custom/Stolas.png
+sudo wget https://github.com/hyprwm/Hyprland/blob/main/assets/install/wall2.png?raw=true -O /usr/share/wallpapers/Custom/hyprchan.png
+
+#Create /crap/ folder 
+sudo mkdir /crap
+sudo chown $USER 777 /crap
+sudo chown 777 /crap
+sudo chmod 777 /crap
+
+echo "Installation is done"
+sleep 5
