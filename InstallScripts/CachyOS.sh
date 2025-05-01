1c3ee50c08e4e94bae8ea8458703a8cee5f0d5b5
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-05-01 21:12:19 +0300

    Update CachyOS.sh

--- InstallScripts/CachyOS.sh
+++ InstallScripts/CachyOS.sh
@@ -12,6 +12,11 @@ sleep 10
 sudo pacman -S flatpak --noconfirm
 sudo pacman -S gnome-disk-utility --noconfirm #DO NOT REMOVE
 
+#Chaotic-aur (replace your /etc/pacman.conf manually too)
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 #Extra flatpak installation folder (CHECK "NOTES/README.MD" FOR MORE INFO)
 sudo mkdir -p /etc/flatpak/installations.d
 
