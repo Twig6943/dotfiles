30b2f53036ab7ef1c3e46a33f8cbfb013c359bc4
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-05-14 21:53:19 +0300

    Update steam.nix

--- NixOS/modules/core/gui/steam.nix
+++ NixOS/modules/core/gui/steam.nix
@@ -7,7 +7,6 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       gamescopeSession.enable = true;
-      platformOptimizations.enable = true;
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
   };
