3a46e83992207fc2533e855b7da1c23ce37ba44b
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-06-10 22:54:38 +0300

    Delete .config/cmus/lib.pl

--- .config/cmus/lib.pl
+++
@@ -1,4 +0,0 @@
-/home/twig/Music/Collection/OSTS/DOOM Eternal OST - Remastered (Fan Made Arrangement)/01. DOOM Eternal.mp3
-/home/twig/Music/Collection/OSTS/DOOM Eternal OST - Remastered (Fan Made Arrangement)/02. S.O.S. from Earth.mp3
-/home/twig/Music/Collection/OSTS/DOOM Eternal OST - Remastered (Fan Made Arrangement)/03. Hell On Earth.mp3
-/home/twig/Music/Collection/OSTS/DOOM Eternal OST - Remastered (Fan Made Arrangement)/04. Deag Nilox - First Priest Death.mp3
