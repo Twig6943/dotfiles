4a8452edd547f3251a7168eb4abe053ea5facf51
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-11-18 11:22:58 +0000

    Update CachyOS.sh

--- installscripts/linux/CachyOS.sh
+++ installscripts/linux/CachyOS.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
 echo "Please update via CachyOS-hello first."
 
-read -n 1 -s
+echo "DONT FORGET TO add;"
+echo "[chaotic-aur]"
+echo "Include = /etc/pacman.d/chaotic-mirrorlist"
+echo "to /etc/pacman.conf"
 
-echo "Please Switch your shell profile to bash."
-
+chsh -s /usr/bin/bash $USER
 read -n 1 -s
 
 sleep 10
@@ -16,6 +19,7 @@ sudo pacman -S gnome-disk-utility --noconfirm #DO NOT REMOVE
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -Syu --noconfirm
 
 #Extra flatpak installation folder (CHECK "NOTES/README.MD" FOR MORE INFO)
 sudo mkdir -p /etc/flatpak/installations.d
@@ -32,7 +36,8 @@ makepkg -si
 #Bash profile fix
 sudo rm ~/.bashrc
 cd ~/
-wget https://raw.githubusercontent.com/Twig6943/dotfiles/refs/heads/main/ShellProfiles/BASH/.bashrc
+wget https://raw.githubusercontent.com/Twig6943/dotfiles/refs/heads/main/.bashrc
+source ~/.bashrc
 
 #Install Soar & am
 yay -S soar-bin am --noconfirm
